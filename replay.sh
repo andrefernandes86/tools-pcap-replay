d553e2c4647dea65ffa7ef9e6e30df767ed2cb4f
Andre Fernandes <andre.fernandes86@gmail.com>
2021-11-27 15:17:36 +0400

    Update replay.sh

--- replay.sh
+++ replay.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-/usr/bin/tcpreplay --intf1=eth0 *.pcap
-/usr/bin/tcpreplay --intf1=eth0 *.pcapng
+/usr/bin/tcpreplay --intf1=eth0 /data/it/*.pcap
+/usr/bin/tcpreplay --intf1=eth0 /data/it/*.pcapng
+/usr/bin/tcpreplay --intf1=eth0 /data/ot/*.pcap
+/usr/bin/tcpreplay --intf1=eth0 /data/ot/*.pcapng
